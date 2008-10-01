8f449a9148300f445ece8fac2aa234bdd7b0c0e1
Valentin Kunz <vale.kunz@googlemail.com>
2008-10-01 14:42:53 +0000

    -a few adds

--- src/include/GNRMain.h
+++ src/include/GNRMain.h
@@ -16,21 +16,33 @@
 #include <wx/statusbr.h>
 //*)
 
+#include <glm.h>
+
+
+#include "TestCanvas.h"
+
 class GNRFrame: public wxFrame
 {
 public:
 
 	GNRFrame(wxWindow* parent,wxWindowID id = -1);
 	virtual ~GNRFrame();
-	
+
+	//void SetCanvas( TestGLCanvas *canvas ) { m_canvas = canvas; }
+    //TestGLCanvas* GetCanvas() { return m_canvas; }
+
+
 private:
+
+	TestCanvas* m_canvas;
 
 	//(*Handlers(GNRFrame)
 	void OnQuit(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
 	void OnRadioBox1Select(wxCommandEvent& event);
+	void OnFileOpen(wxCommandEvent& event);
 	//*)
-	
+
 	//(*Identifiers(GNRFrame)
 	static const long idMenuLoad;
 	static const long idMenuSave;
@@ -39,11 +51,13 @@ private:
 	static const long idMenuAbout;
 	static const long ID_STATUSBAR1;
 	//*)
-	
+
 	//(*Declarations(GNRFrame)
+	wxMenuItem* MenuItem5;
+	wxMenuItem* MenuItem3;
 	wxStatusBar* StatusBar1;
 	//*)
-	
+
 	DECLARE_EVENT_TABLE()
 };
 
