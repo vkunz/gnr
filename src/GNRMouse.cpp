0b0e4a1350c4b6e9698c286d593eb9e1eb5511ef
Patrick Kracht <patrick.kracht@gmail.com>
2008-10-09 12:38:05 +0000

    mouse class modified;

--- src/GNRMouse.cpp
+++ src/GNRMouse.cpp
@@ -7,26 +7,95 @@
 
 GNRMouse::GNRMouse()
 {
+	Init(1.0f, 1000.0f);
+}
+
+GNRMouse::GNRMouse(float m_znear, float m_zfar)
+{
+	Init(m_znear, m_zfar);
 }
 
 GNRMouse::~GNRMouse()
 {
 }
 
-void GNRMouse::ObjectRotate(int m_x, int m_y)
+void GNRMouse::Init(float m_znear, float m_zfar)
 {
-	phiy  = phiy_old + 720.0*((float)(m_mouse_x - m_x)/(float)window_w);
-	phix  = phix_old + 720.0*((float)(m_mouse_y - m_y)/(float)window_h);
+	in_use = false;
+	
+	status = MOVEXZ;
+	
+	phix   = phix_old = 0.0f;
+	phiy   = phiy_old = 0.0f;
+	phiz   = angle    = 0.0f;
+	
+	zfar   = m_zfar;
+	znear  = m_znear;
 }
 
-void GNRMouse::ObjectMoveXY(int m_x, int m_y)
+bool GNRMouse::GetControl(wxMouseEvent& event)
 {
-	posx -= (float)(m_mouse_x - m_x)/300.0*fabs(posz);
-	posy -= (float)(m_y - m_mouse_y)/300.0*fabs(posz);
+	if (in_use)
+	{
+		return false;
+	}
+	
+//TODO
+//    my_object = assembly;
+
+	//store actual mouse coords on control
+	m_mouse_x = event.m_x;
+	m_mouse_y = event.m_y;
+	
+	in_use = true;
+	return true;
 }
 
-void GNRMouse::ObjectMoveXZ(int m_x, int m_y)
+bool GNRMouse::DropControl(wxMouseEvent& event)
 {
-	posx -= (float)(m_mouse_x - m_x)/360.0*fabs(posz);
-	posz -= (float)(m_mouse_y - m_y)/10.0;
+//TODO
+//    if ( in_use && my_object == assembly )
+//    {
+//        my_object = NULL;
+//        in_use = false;
+//    }
+	in_use = false;
+	return !in_use;
+}
+
+void GNRMouse::ObjectTransform(wxMouseEvent& event)
+{
+	switch (status)
+	{
+	case MOVEXZ:
+		ObjectMoveXZ(event);
+		break;
+	case MOVEXY:
+		ObjectMoveXY(event);
+		break;
+	case ROTATE:
+		ObjectRotate(event);
+		break;
+	default:
+		ObjectMoveXZ(event);
+		break;
+	}
+}
+
+void GNRMouse::ObjectRotate(wxMouseEvent& event)
+{
+	phiy  = phiy_old + 720.0*((float)(m_mouse_x - event.m_x)/(float)window_w);
+	phix  = phix_old + 720.0*((float)(m_mouse_y - event.m_y)/(float)window_h);
+}
+
+void GNRMouse::ObjectMoveXY(wxMouseEvent& event)
+{
+	posx -= (float)(m_mouse_x - event.m_x)/300.0*fabs(posz);
+	posy -= (float)(event.m_y - m_mouse_y)/300.0*fabs(posz);
+}
+
+void GNRMouse::ObjectMoveXZ(wxMouseEvent& event)
+{
+	posx -= (float)(m_mouse_x - event.m_x)/360.0*fabs(posz);
+	posz -= (float)(m_mouse_y - event.m_y)/10.0;
 }
--- src/include/GNRMouse.h
+++ src/include/GNRMouse.h
@@ -1,32 +1,70 @@
 #ifndef _GNRMOUSE_H_
 #define _GNRMOUSE_H_
 
+#include <wx/dcclient.h>
+#include <wx/msgdlg.h>
+#include <wx/string.h>
+#include <wx/timer.h>
+
+#include "GNRAssembly.h"
+
 class GNRMouse
 {
 private:
-	bool m_LMousePressed, m_MMousePressed, m_RMousePressed;
+
+	//action to do
+	enum { MOVEXZ = 0, MOVEXY, ROTATE };
+	int status;
 	
+	//remember if in use or not
+	bool in_use;
+	
+	//old mouse coords on initialized dragging
 	int	m_mouse_x, m_mouse_y;
-	int window_w, window_h;
-	int zcoord;
 	
+	//window dimensions of actual frame
+	int window_w, window_h;
+	
+	//saved positions in 3d canvas frame (later stored in assembly)
 	float posx, posy, posz;
 	float phix, phiy, phiz, angle;
 	float phix_old, phiy_old;
 	float zfar, znear;
 	
-	GNRMouse();
-	virtual ~GNRMouse();
+	//pointer to assembly
+	GNRAssembly* my_object;
 	
 protected:
 
-	void ObjectRotate(int m_x, int m_y);
-	void ObjectMoveXY(int m_x, int m_y);
-	void ObjectMoveXZ(int m_x, int m_y);
+	//action for rotating asembly
+	void ObjectRotate(wxMouseEvent& event);
+	
+	//action for moving asembly in XY
+	void ObjectMoveXY(wxMouseEvent& event);
+	
+	//action for moving asembly in XZ (default)
+	void ObjectMoveXZ(wxMouseEvent& event);
 	
 public:
 
-
+	void Init(float m_zfar, float m_znear);
+	bool IsInUse()
+	{
+		return in_use;
+	};
+	
+	//fetching control of mouse by event
+	bool GetControl(wxMouseEvent& event);
+	bool DropControl(wxMouseEvent& event);
+	void ObjectTransform(wxMouseEvent& event);
+	
+	//ctor
+	GNRMouse();
+	GNRMouse(float m_zfar, float m_znear);
+	
+	//dtor
+	virtual ~GNRMouse();
+	
 };
 
 #endif // _GNRMOUSE_H_
