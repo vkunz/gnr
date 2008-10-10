95644382af9afb100d6278e694c888e7b0a44b51
Patrick Kracht <patrick.kracht@gmail.com>
2008-10-10 06:08:00 +0000

    GNRApp comments added;

--- src/GNRApp.cpp
+++ src/GNRApp.cpp
@@ -1,11 +1,13 @@
-/***************************************************************
- * Name:      GNRApp.cpp
- * Purpose:   Code for Application Class
- * Author:    Patrick Kracht (patrick.kracht@googlemail.com)
- * Created:   2008-09-30
- * Copyright: Patrick Kracht (http://www.omega2k.de)
- * License:
- **************************************************************/
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * code for application class
+ * @name        GNRApp.cpp
+ * @date        2008-09-30
+ * @author		Konstantin Balabin  <k.balabin@googlemail.com>
+ * @author		Patrick Kracht      <patrick.kracht@googlemail.com>
+ * @author		Thorsten Moll       <thorsten.moll@googlemail.com>
+ * @author		Valentin Kunz       <athostr@googlemail.com>
+ */
 
 #include <wx/image.h>
 
