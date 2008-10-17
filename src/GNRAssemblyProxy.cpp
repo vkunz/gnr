5e181aa8bcc3efec4d646b6a42d12b2611aa05da
Patrick Kracht <patrick.kracht@gmail.com>
2008-10-17 10:21:26 +0000

    PROBLEM: assembly pointer cast to int is evil! recast in proxy is dangerous!

--- src/GNRAssemblyProxy.cpp
+++ src/GNRAssemblyProxy.cpp
@@ -5,10 +5,10 @@
  * for the glcanvas frames
  * @name        GNRAssemblyProxy.cpp
  * @date        2008-10-09
- * @author		Konstantin Balabin  <k.balabin@googlemail.com>
- * @author		Patrick Kracht      <patrick.kracht@googlemail.com>
- * @author		Thorsten Moll       <thorsten.moll@googlemail.com>
- * @author		Valentin Kunz       <athostr@googlemail.com>
+ * @author              Konstantin Balabin  <k.balabin@googlemail.com>
+ * @author              Patrick Kracht      <patrick.kracht@googlemail.com>
+ * @author              Thorsten Moll       <thorsten.moll@googlemail.com>
+ * @author              Valentin Kunz       <athostr@googlemail.com>
  */
 
 #include "GNRAssemblyProxy.h"
@@ -43,10 +43,10 @@ GNRAssemblyProxy::~GNRAssemblyProxy()
  * @param       int         height of frame
  * @access      public
  */
-void GNRAssemblyProxy::setWindow(int width, int height)
+void GNRAssemblyProxy::setWindow(GNRGLNotifyEvent& event)
 {
-	window_w = width;
-	window_h = height;
+	window_w = event.getWinX();
+	window_h = event.getWinY();
 }
 
 /**
@@ -69,7 +69,7 @@ void GNRAssemblyProxy::init()
  * @return      boolean         status of get action
  * @access      public
  */
-bool GNRAssemblyProxy::getControl(GNRAssembly* object, wxMouseEvent& event)
+bool GNRAssemblyProxy::getControl(GNRGLNotifyEvent& event)
 {
 	//if in use return false
 	if (in_use)
@@ -80,7 +80,7 @@ bool GNRAssemblyProxy::getControl(GNRAssembly* object, wxMouseEvent& event)
 	in_use = true;
 	
 	//store object pointer
-	my_object = object;
+	my_object = (GNRAssembly*) event.getSelectedObj();
 	
 	//store old rotation values
 	phi_old   = my_object->getPhi();
@@ -88,8 +88,8 @@ bool GNRAssemblyProxy::getControl(GNRAssembly* object, wxMouseEvent& event)
 	rho_old   = my_object->getRho();
 	
 	//store actual mouse coords on control
-	m_mouse_x = event.m_x;
-	m_mouse_y = event.m_y;
+	m_mouse_x = event.getMouseEvent().m_x;
+	m_mouse_y = event.getMouseEvent().m_y;
 	
 	return true;
 }
@@ -99,7 +99,7 @@ bool GNRAssemblyProxy::getControl(GNRAssembly* object, wxMouseEvent& event)
  * @param       wxMouseEvent    mouse event of current frame
  * @access      public
  */
-void GNRAssemblyProxy::dropControl(wxMouseEvent& event)
+void GNRAssemblyProxy::dropControl(GNRGLNotifyEvent& event)
 {
 	//set in use false
 	in_use = false;
@@ -110,7 +110,7 @@ void GNRAssemblyProxy::dropControl(wxMouseEvent& event)
  * @param       wxMouseEvent    mouse event of current frame
  * @access      public
  */
-void GNRAssemblyProxy::ObjectTransform(wxMouseEvent& event)
+void GNRAssemblyProxy::ObjectTransform(GNRGLNotifyEvent& event)
 {
 	if (in_use)
 	{
@@ -137,10 +137,10 @@ void GNRAssemblyProxy::ObjectTransform(wxMouseEvent& event)
  * @param       wxMouseEvent    mouse event of current frame
  * @access      protected
  */
-void GNRAssemblyProxy::ObjectRotate(wxMouseEvent& event)
+void GNRAssemblyProxy::ObjectRotate(GNRGLNotifyEvent& event)
 {
-	my_object->setPhi(phi_old + 720.0*((float)(m_mouse_y - event.m_y)/(float)window_h));
-	my_object->setTheta(theta_old + 720.0*((float)(m_mouse_x - event.m_x)/(float)window_w));
+	my_object->setPhi(phi_old + 720.0*((float)(m_mouse_y - event.getMouseEvent().m_y)/(float)window_h));
+	my_object->setTheta(theta_old + 720.0*((float)(m_mouse_x - event.getMouseEvent().m_x)/(float)window_w));
 }
 
 /**
@@ -148,10 +148,10 @@ void GNRAssemblyProxy::ObjectRotate(wxMouseEvent& event)
  * @param       wxMouseEvent    mouse event of current frame
  * @access      protected
  */
-void GNRAssemblyProxy::ObjectMoveXY(wxMouseEvent& event)
+void GNRAssemblyProxy::ObjectMoveXY(GNRGLNotifyEvent& event)
 {
-	my_object->setX(my_object->getX() - (float)(m_mouse_x - event.m_x)/300.0*fabs(my_object->getZ()));
-	my_object->setY(my_object->getY() - (float)(event.m_y - m_mouse_y)/300.0*fabs(my_object->getZ()));
+	my_object->setX(my_object->getX() - (float)(m_mouse_x - event.getMouseEvent().m_x)/300.0*fabs(my_object->getZ()));
+	my_object->setY(my_object->getY() - (float)(event.getMouseEvent().m_y - m_mouse_y)/300.0*fabs(my_object->getZ()));
 }
 
 /**
@@ -159,10 +159,10 @@ void GNRAssemblyProxy::ObjectMoveXY(wxMouseEvent& event)
  * @param       wxMouseEvent    mouse event of current frame
  * @access      protected
  */
-void GNRAssemblyProxy::ObjectMoveXZ(wxMouseEvent& event)
+void GNRAssemblyProxy::ObjectMoveXZ(GNRGLNotifyEvent& event)
 {
-	my_object->setX(my_object->getX() - (float)(m_mouse_x - event.m_x)/360.0*fabs(my_object->getZ()));
-	my_object->setZ(my_object->getZ() - (float)(m_mouse_y - event.m_y)/10.0);
+	my_object->setX(my_object->getX() - (float)(m_mouse_x - event.getMouseEvent().m_x)/360.0*fabs(my_object->getZ()));
+	my_object->setZ(my_object->getZ() - (float)(m_mouse_y - event.getMouseEvent().m_y)/10.0);
 }
 
 /**
