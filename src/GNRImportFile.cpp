5618caceb97ccc983cebcb723ba307c5cec52c33
K.Balabin <alfur@users.noreply.github.com>
2008-10-22 09:24:35 +0000

    missing files added

---
+++ src/GNRImportFile.cpp
@@ -0,0 +1,20 @@
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * GNRImportFile
+ * @name        GNRImportFile.h
+ * @date        2008-10-05
+ * @author		Konstantin Balabin  <k.balabin@googlemail.com>
+ * @author		Patrick Kracht      <patrick.kracht@googlemail.com>
+ * @author		Thorsten Moll       <thorsten.moll@googlemail.com>
+ * @author		Valentin Kunz       <athostr@googlemail.com>
+ */
+
+#include "GNRImportFile.h"
+
+GNRImportFile::GNRImportFile()
+{
+}
+
+GNRImportFile::~GNRImportFile()
+{
+}
