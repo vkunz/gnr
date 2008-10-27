2dc052db81f519bb5ccdb395326dbffbcdf1092d
Valentin Kunz <vale.kunz@googlemail.com>
2008-10-27 13:24:28 +0000

    - new ExampleProject.opx, added 31...ce.oax contains only primitives - extend batch-script to build 31...ce.oax, too - hopefully ever xml is in utf-8 now, DO NOT OPEN WITH CODEBLOCKS, use editors, which save them in utf-8 again

--- data/CreateReferences.bat
+++ data/CreateReferences.bat
@@ -15,11 +15,16 @@ cd 31696c04-386a-4407-8f3e-c3f6e92d91cd
 7z a -tzip 31696c04-386a-4407-8f3e-c3f6e92d91cd.oax stardestroyer.mtl stardestroyer.obj content.xml
 
 cd ..
+cd 31696c04-386a-4407-8f3e-c3f6e92d91ce
+7z a -tzip 31696c04-386a-4407-8f3e-c3f6e92d91ce.oax content.xml
+
+cd ..
 
 move /Y 31696c04-386a-4407-8f3e-c3f6e92d91ca\31696c04-386a-4407-8f3e-c3f6e92d91ca.oax
 move /Y 31696c04-386a-4407-8f3e-c3f6e92d91cb\31696c04-386a-4407-8f3e-c3f6e92d91cb.oax
 move /Y 31696c04-386a-4407-8f3e-c3f6e92d91cc\31696c04-386a-4407-8f3e-c3f6e92d91cc.oax
 move /Y 31696c04-386a-4407-8f3e-c3f6e92d91cd\31696c04-386a-4407-8f3e-c3f6e92d91cd.oax
+move /Y 31696c04-386a-4407-8f3e-c3f6e92d91ce\31696c04-386a-4407-8f3e-c3f6e92d91ce.oax
 
 cd ..
 cd Reference-opx
@@ -28,6 +33,7 @@ copy /Y .\..\Reference-oax\31696c04-386a-4407-8f3e-c3f6e92d91ca.oax .\ExamplePro
 copy /Y .\..\Reference-oax\31696c04-386a-4407-8f3e-c3f6e92d91cb.oax .\ExampleProject\assemblies
 copy /Y .\..\Reference-oax\31696c04-386a-4407-8f3e-c3f6e92d91cc.oax .\ExampleProject\assemblies
 copy /Y .\..\Reference-oax\31696c04-386a-4407-8f3e-c3f6e92d91cd.oax .\ExampleProject\assemblies
+copy /Y .\..\Reference-oax\31696c04-386a-4407-8f3e-c3f6e92d91ce.oax .\ExampleProject\assemblies
 
 cd ExampleProject 
 7z a -tzip ExampleProject.opx content.xml assemblies\31696c04-386a-4407-8f3e-c3f6e92d91ca.oax assemblies\31696c04-386a-4407-8f3e-c3f6e92d91cb.oax assemblies\31696c04-386a-4407-8f3e-c3f6e92d91cc.oax assemblies\31696c04-386a-4407-8f3e-c3f6e92d91cd.oax 
