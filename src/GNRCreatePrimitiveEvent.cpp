10866762cf7728c3525de8c26daf3d88c4aa8361
thorsten.moll <thorsten.moll@googlemail.com>
2008-10-31 13:06:45 +0000

    added CreatePrimitiveEvent

---
+++ src/GNRCreatePrimitiveEvent.cpp
@@ -0,0 +1,47 @@
+#include "GNRCreatePrimitiveEvent.h"
+
+DEFINE_EVENT_TYPE(wxEVT_COMMAND_GNR_CREATE_PRIMITIVE)
+IMPLEMENT_DYNAMIC_CLASS(GNRCreatePrimitiveEvent, wxNotifyEvent)
+
+
+GNRCreatePrimitiveEvent::GNRCreatePrimitiveEvent(wxEventType commandType, int id)
+		: wxNotifyEvent(commandType, id) {}
+
+GNRCreatePrimitiveEvent::GNRCreatePrimitiveEvent(const GNRCreatePrimitiveEvent& event): wxNotifyEvent(event) {}
+
+wxEvent* GNRCreatePrimitiveEvent::Clone() const
+{
+	return new GNRCreatePrimitiveEvent(*this);
+}
+
+GNRCreatePrimitiveEvent::~GNRCreatePrimitiveEvent() {}
+
+void GNRCreatePrimitiveEvent::setPosition(GNRVertex pos)
+{
+	m_position = pos;
+}
+
+void GNRCreatePrimitiveEvent::setAngles(GNRVertex angles)
+{
+	m_angles = angles;
+}
+
+void GNRCreatePrimitiveEvent::setDimensions(GNRVertex dim)
+{
+	m_dimensions = dim;
+}
+
+GNRVertex GNRCreatePrimitiveEvent::getPosition()
+{
+	return m_position;
+}
+
+GNRVertex GNRCreatePrimitiveEvent::getAngles()
+{
+	return m_angles;
+}
+
+GNRVertex GNRCreatePrimitiveEvent::getDimensions()
+{
+	return m_dimensions;
+}
---
+++ src/include/GNRCreatePrimitiveEvent.h
@@ -0,0 +1,49 @@
+#ifndef GNRCREATEPRIMITIVEEVENT_H
+#define GNRCREATEPRIMITIVEEVENT_H
+
+#include <wx/event.h>
+#include "GNREnum.h"
+#include "GNRVertex.h"
+
+class GNRCreatePrimitiveEvent : public wxNotifyEvent
+{
+private:
+	primitivesType m_type;
+	GNRVertex m_position;
+	GNRVertex m_angles;
+	GNRVertex m_dimensions;
+	
+public:
+	GNRCreatePrimitiveEvent(wxEventType commandType = wxEVT_NULL, int id = 0);
+	GNRCreatePrimitiveEvent(const GNRCreatePrimitiveEvent& event);
+	virtual wxEvent* Clone() const;
+	virtual ~GNRCreatePrimitiveEvent();
+	
+	void setPrimitiveType(primitivesType type);
+	
+	void setPosition(GNRVertex pos);
+	void setAngles(GNRVertex angles);
+	void setDimensions(GNRVertex dim);
+	
+	primitivesType getPrimitiveTypes();
+	
+	GNRVertex getPosition();
+	GNRVertex getAngles();
+	GNRVertex getDimensions();
+	
+	DECLARE_DYNAMIC_CLASS(GNRCreatePrimitiveEvent);
+};
+
+typedef void (wxEvtHandler::*GNRCreatePrimitiveEventFunction)(GNRCreatePrimitiveEvent&);
+
+// GNRGLNotify events and macros for handling them
+BEGIN_DECLARE_EVENT_TYPES()
+DECLARE_EVENT_TYPE(wxEVT_COMMAND_GNR_CREATE_PRIMITIVE, 805)
+END_DECLARE_EVENT_TYPES()
+
+#define EVT_GNR_CREATE_PRIMITIVE(id, fn) DECLARE_EVENT_TABLE_ENTRY( \
+wxEVT_COMMAND_GNR_CREATE_PRIMITIVE, id, -1, (wxObjectEventFunction) \
+(wxEventFunction) (GNRCreatePrimitiveEventFunction) & fn, \
+(wxObject *) NULL ),
+
+#endif // GNRCREATEPRIMITIVEEVENT_H
