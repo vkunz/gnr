995b38b6165add3d1eacca5a9fb3cb13b783e872
thorsten.moll <thorsten.moll@googlemail.com>
2008-11-08 17:37:29 +0000

    removed GNRSceneTreeNode (not needed any more)

--- src/include/GNRAssemblyDataFrame.h
+++ src/include/GNRAssemblyDataFrame.h
@@ -13,43 +13,43 @@ class GNRAssemblyDataFrame: public wxFrame
 {
 public:
 
-	GNRAssemblyDataFrame(wxWindow* parent = 0,wxWindowID id=1,const wxPoint& pos=wxDefaultPosition,const wxSize& size=wxDefaultSize);
+	GNRAssemblyDataFrame(wxWindow* parent = 0, wxWindowID id=1);
 	virtual ~GNRAssemblyDataFrame();
-	
+
 	void fillFields(GNRAssembly* assembly);
-	
+
 protected:
 
 private:
 	// assembly-sizes
 	float m_assemblyWidth, m_assemblyHeight, m_assemblyDepth;
-	
+
 	GNRAssembly* m_assembly;
-	
+
 	// text boxes
 	wxTextCtrl* m_txcName;
-	
+
 	wxTextCtrl* m_txcWidth;
 	wxTextCtrl* m_txcHeight;
 	wxTextCtrl* m_txcDepth;
-	
+
 	wxCheckBox* m_cbxVisible;
 	wxCheckBox* m_cbxProportion;
-	
+
 	// buttons
 	wxButton* m_btnChange;
 	wxButton* m_btnCancel;
-	
+
 	// text labels
 	wxStaticText* m_stxName;
-	
+
 	wxStaticText* m_stxWidth;
 	wxStaticText* m_stxHeight;
 	wxStaticText* m_stxDepth;
-	
+
 	wxStaticText* m_stxVisible;
 	wxStaticText* m_stxProportion;
-	
+
 	// ids
 	static const long idtxcName;
 	static const long idtxcWidth;
@@ -59,14 +59,14 @@ private:
 	static const long idcbxProportion;
 	static const long idBtnChange;
 	static const long idBtnCancel;
-	
+
 	void OnChange(wxCommandEvent& WXUNUSED(event));
 	void OnCancel(wxCommandEvent& WXUNUSED(event));
 	void OnClose(wxCloseEvent& WXUNUSED(event));
 	void OnWidthChange(wxCommandEvent& event);
 	void OnHeightChange(wxCommandEvent& event);
 	void OnDepthChange(wxCommandEvent& event);
-	
+
 	DECLARE_EVENT_TABLE()
 };
 
