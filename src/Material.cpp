e8c2007394755bde5aef87f8a013eab15de6629e
Valentin Kunz <vale.kunz@googlemail.com>
2010-10-10 13:39:11 +0000

    Include/declaration cleanup for src/Material.cpp

--- src/Material.cpp
+++ src/Material.cpp
@@ -1,13 +1,18 @@
-#include "Material.h"
-
-#include <GL/gl.h>
+/**
+ * Material
+ * @name        Material.cpp
+ * @date        2008-09-30
+ * @author		Konstantin Balabin  <k.balabin@googlemail.com>
+ * @author		Patrick Kracht      <patrick.kracht@googlemail.com>
+ * @author		Thorsten Moll       <thorsten.moll@googlemail.com>
+ * @author		Valentin Kunz       <athostr@googlemail.com>
+ */
 
 #include <sstream>
 #include <iostream>
+#include <GL/gl.h>
 
-using std::stringstream;
-using std::endl;
-
+#include "Material.h"
 
 Material::Material():
 		m_shininess(80), m_alpha(1.0f)
@@ -81,26 +86,26 @@ void Material::set() const
 {
 	float val[4];
 	val[3] = 1.0-m_alpha;
-	
+
 	m_ambient.getAll(val[0], val[1], val[2]);
 	glMaterialfv(GL_FRONT, GL_AMBIENT, val);
-	
+
 	m_diffuse.getAll(val[0], val[1], val[2]);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, val);
-	
+
 	m_specular.getAll(val[0], val[1], val[2]);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, val);
-	
+
 	glMateriali(GL_FRONT, GL_SHININESS, m_shininess);
 }
 
-ostream& operator<< (ostream& out, const Material &m)
+std::ostream& operator<<(std::ostream& out, const Material &m)
 {
-	out << "Ka " << m.m_ambient << endl;
-	out << "Kd " << m.m_diffuse << endl;
-	out << "Ks " << m.m_specular << endl;
-	out << "d " << m.m_alpha << endl;
-	out << "Ns " << m.m_shininess << endl;
-	
+	out << "Ka " << m.m_ambient << std::endl;
+	out << "Kd " << m.m_diffuse << std::endl;
+	out << "Ks " << m.m_specular << std::endl;
+	out << "d " << m.m_alpha << std::endl;
+	out << "Ns " << m.m_shininess << std::endl;
+
 	return out;
 }
--- src/include/Material.h
+++ src/include/Material.h
@@ -1,37 +1,46 @@
+/**
+ * Material
+ * @name                Material.h
+ * @date                2008-10-05
+ * @author		Konstantin Balabin  <k.balabin@googlemail.com>
+ * @author		Patrick Kracht      <patrick.kracht@googlemail.com>
+ * @author		Thorsten Moll       <thorsten.moll@googlemail.com>
+ * @author		Valentin Kunz       <athostr@googlemail.com>
+ */
+
 #ifndef __MATERIAL_H__
 #define __MATERIAL_H__
 
-#include "Color.h"
-
+#include <iostream>
 #include <string>
 
-using std::string;
+#include "Color.h"
 
 class Material
 {
 public:
-	friend ostream& operator<< (ostream& out, const Material &m);
-	
+	friend std::ostream& operator<<(std::ostream& out, const Material &m);
+
 	Material();
 	Material(const Material& other);
 	virtual ~Material();
-	
+
 	const Color& Ambient() const;
 	const Color& Diffuse() const;
 	const Color& Specular() const;
-	
+
 	Color& Ambient();
 	Color& Diffuse();
 	Color& Specular();
-	
+
 	const int& Shininess() const;
 	int& Shininess();
-	
+
 	const float& Alpha() const;
 	float& Alpha();
-	
+
 	void set() const;
-	
+
 private:
 	Color m_ambient, m_diffuse, m_specular;
 	int m_shininess;
