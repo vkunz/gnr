a102ebfadc92b50ea860f3fc4700c56835366330
Valentin Kunz <vale.kunz@googlemail.com>
2010-10-10 13:25:53 +0000

    Include/declaration cleanup for src/include/AssemblyData.h

--- src/include/AssemblyData.h
+++ src/include/AssemblyData.h
@@ -8,32 +8,32 @@
  * @author		Valentin Kunz       <athostr@googlemail.com>
  */
 
-#ifndef _GNRFRAMEDATA_H_
-#define _GNRFRAMEDATA_H_
+#ifndef _FRAMEDATA_H_
+#define _FRAMEDATA_H_
 
 #include <list>
-#include <wx/string.h>
+//#include <wx/string.h>
 
 struct AssemblyData
 {
 	wxString m_name;
 	wxString m_category;
 	unsigned int m_category_id;
-	
+
 	double m_width;
 	double m_depth;
 	double m_height;
-	
+
 	double m_scaleWidth;
 	double m_scaleDepth;
 	double m_scaleHeight;
-	
+
 	double m_offsetX;
 	double m_offsetY;
 	double m_offsetZ;
-	
+
 	std::list<wxString> m_listFiles;
 	std::list<wxString> m_tags;
 };
 
-#endif // _GNRFRAMEDATA_H_
+#endif // _FRAMEDATA_H_
