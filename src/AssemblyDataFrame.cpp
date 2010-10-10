1605479791957b3de9026dd2cd3ab97f612b0f9d
Valentin Kunz <vale.kunz@googlemail.com>
2010-10-10 13:25:05 +0000

    Include/declaration cleanup for src/AssemblyDataFrame.cpp/h

--- src/AssemblyDataFrame.cpp
+++ src/AssemblyDataFrame.cpp
@@ -9,23 +9,21 @@
  * @author		Valentin Kunz       <athostr@googlemail.com>
  */
 
-#include "include/AssemblyDataFrame.h"
-
 #include <wx/intl.h>
 #include <wx/string.h>
 #include <wx/valtext.h>
 #include <wx/icon.h>
 
-#include "NotifyEvent.h"
-#include "GlobalDefine.h"
-
 #if defined(__ATHOS_DEBUG__)
 #include <wx/log.h>
 #endif
 
-#if !defined(__WXMSW__) && !defined(__WXPM__)
+#include "Assembly.h"
+#include "AssemblyDataFrame.h"
+#include "GlobalDefine.h"
+#include "NotifyEvent.h"
 #include "resources/gnr_icon.xpm"
-#endif
+
 
 const long AssemblyDataFrame::idtxcName = wxNewId();
 const long AssemblyDataFrame::idSpcWidth = wxNewId();
@@ -56,48 +54,48 @@ AssemblyDataFrame::AssemblyDataFrame(wxWindow* parent,wxWindowID id)
 	CentreOnParent(wxBOTH);
 	SetBackgroundColour(wxNullColour);
 	SetIcon(wxICON(gnr_icon));
-	
+
 	m_panel = new wxPanel(this, wxID_ANY, wxPoint(0, 0), wxSize(350, 300), wxTAB_TRAVERSAL);
-	
+
 	m_stxName = new wxStaticText(m_panel, -1, wxT("Name:"), wxPoint(30,34), wxSize(80,22), 0);
 	m_txcName = new wxTextCtrl(m_panel, idtxcName, wxT(""), wxPoint(120,30), wxSize(150,22), 0, wxDefaultValidator);
-	
+
 	m_stxWidth = new wxStaticText(m_panel, -1, wxT("Breite (mm):"), wxPoint(30,84), wxSize(80,22), 0);
 	//m_txcWidth = new wxTextCtrl(this, idtxcWidth, wxT(""), wxPoint(120,80), wxSize(150,22), 0, wxTextValidator(wxFILTER_NUMERIC));
 	m_txcWidth = new wxSpinCtrl(m_panel, idSpcWidth, wxT(""), wxPoint(120,80), wxSize(150,22), wxTE_PROCESS_ENTER, SIZE_MINIMUM_VALUE, SIZE_MAXIMUM_VALUE, SIZE_MINIMUM_VALUE);
-	
+
 	m_stxHeight = new wxStaticText(m_panel, -1, wxT("H" ouml "he (mm):"), wxPoint(30,114), wxSize(80,22), 0);
 	//m_txcHeight = new wxTextCtrl(this, idtxcHeight, wxT(""), wxPoint(120,110), wxSize(150,22), 0, wxTextValidator(wxFILTER_NUMERIC));
 	m_txcHeight = new wxSpinCtrl(m_panel, idSpcHeight, wxT(""), wxPoint(120,110), wxSize(150,22), wxTE_PROCESS_ENTER, SIZE_MINIMUM_VALUE, SIZE_MAXIMUM_VALUE, SIZE_MINIMUM_VALUE);
-	
+
 	m_stxDepth = new wxStaticText(m_panel, -1, wxT("Tiefe (mm):"), wxPoint(30,144), wxSize(80,22), 0);
 	//m_txcDepth = new wxTextCtrl(this, idtxcDepth, wxT(""), wxPoint(120,140), wxSize(150,22), 0, wxTextValidator(wxFILTER_NUMERIC));
 	m_txcDepth = new wxSpinCtrl(m_panel, idSpcDepth, wxT(""), wxPoint(120,140), wxSize(150,22), wxTE_PROCESS_ENTER, SIZE_MINIMUM_VALUE, SIZE_MAXIMUM_VALUE, SIZE_MINIMUM_VALUE);
-	
+
 	m_cbxProportion = new wxCheckBox(m_panel, idcbxProportion, wxT("Proportionen beibehalten"), wxPoint(120, 170), wxSize(160, 22));
 	m_cbxProportion->SetValue(true);
-	
+
 	m_stxVisible = new wxStaticText(m_panel, -1, wxT("Sichtbar:"), wxPoint(30,212), wxSize(80,22), 0);
 	m_cbxVisible = new wxCheckBox(m_panel, idcbxVisible, wxEmptyString, wxPoint(120, 210), wxSize(80, 22));
-	
+
 	m_btnChange  = new wxButton(m_panel, idBtnChange, wxT("" Auml "ndern"), wxPoint(70,255), wxDefaultSize, 0);
 	m_btnCancel  = new wxButton(m_panel, idBtnCancel, wxT("Abbrechen"), wxPoint(190,255), wxDefaultSize, 0);
-	
+
 	// connects m_spcWidth with OnSpcWidthChange
 	Connect(idSpcWidth,wxEVT_COMMAND_SPINCTRL_UPDATED,(wxObjectEventFunction)&AssemblyDataFrame::OnWidthChange);
-	
+
 	// connects m_spcDepth with OnSpcDepthChange
 	Connect(idSpcDepth,wxEVT_COMMAND_SPINCTRL_UPDATED,(wxObjectEventFunction)&AssemblyDataFrame::OnDepthChange);
-	
+
 	// connects m_spcHeight with OnSpcHeightChange
 	Connect(idSpcHeight,wxEVT_COMMAND_SPINCTRL_UPDATED,(wxObjectEventFunction)&AssemblyDataFrame::OnHeightChange);
-	
+
 	// connects m_spcWidth with OnSpcWidthChange on press Enter
 	Connect(idSpcWidth,wxEVT_COMMAND_TEXT_ENTER,(wxObjectEventFunction)&AssemblyDataFrame::OnWidthChange);
-	
+
 	// connects m_spcDepth with OnSpcDepthChange on press Enter
 	Connect(idSpcDepth,wxEVT_COMMAND_TEXT_ENTER,(wxObjectEventFunction)&AssemblyDataFrame::OnDepthChange);
-	
+
 	// connects m_spcHeight with OnSpcHeightChange on press Enter
 	Connect(idSpcHeight,wxEVT_COMMAND_TEXT_ENTER,(wxObjectEventFunction)&AssemblyDataFrame::OnHeightChange);
 }
@@ -114,26 +112,26 @@ AssemblyDataFrame::~AssemblyDataFrame() {}
 void AssemblyDataFrame::fillFields(Assembly* assembly)
 {
 	m_assembly = assembly;
-	
+
 	wxString str;
-	
+
 	m_txcName->ChangeValue(assembly->getName());
-	
+
 	float x, y, z;
-	
+
 	assembly->world_dimension().getAll(x, y, z);
 	m_txcWidth->SetValue((int)(x * 1000.0f));
 	m_txcHeight->SetValue((int)(y * 1000.0f));
 	m_txcDepth->SetValue((int)(z * 1000.0f));
-	
+
 	assembly->dimension().getAll(x, y, z);
 	m_assemblyWidth  = (int)(x  * 1000);
 	m_assemblyHeight = (int)(y * 1000);
 	m_assemblyDepth  = (int)(z  * 1000);
-	
-	
+
+
 	m_cbxVisible->SetValue(m_assembly->isVisible());
-	
+
 }
 
 /**
@@ -144,37 +142,37 @@ void AssemblyDataFrame::fillFields(Assembly* assembly)
 void AssemblyDataFrame::OnChange(wxCommandEvent& WXUNUSED(event))
 {
 	m_assembly->setName(m_txcName->GetValue());
-	
+
 	float factor = (float)m_txcWidth->GetValue() / m_assemblyWidth;
 	m_assembly->scale().setX((factor));
-	
+
 	//fix for keeping bottom of object on same level
 	float old_h = m_assembly->world_dimension().getY();
 	float old_y = m_assembly->position().getY();
-	
+
 	//calculate new scale
 	factor = (float)m_txcHeight->GetValue() / m_assemblyHeight;
 	m_assembly->scale().setY((factor));
-	
+
 	//now correct y-level of center
 	m_assembly->position().setY(old_y + (m_assembly->world_dimension().getY() - old_h) / 2.0f);
-	
+
 	factor = (float)m_txcDepth->GetValue() / m_assemblyDepth;
 	m_assembly->scale().setZ(factor);
-	
+
 	m_assembly->setVisible(m_cbxVisible->IsChecked());
-	
+
 	// send event to refresh canvas
 	NotifyEvent myevent(wxEVT_COMMAND_GNR_NOTIFY);
 	myevent.setGNREventType(GLREFRESH);
 	myevent.SetEventObject(this);
 	GetEventHandler()->ProcessEvent(myevent);
-	
+
 	// send event to refresh tree
 	myevent.setGNREventType(REFRESHSCENETREE);
 	myevent.SetEventObject(this);
 	GetEventHandler()->ProcessEvent(myevent);
-	
+
 	MakeModal(false);
 	Destroy();
 }
@@ -208,7 +206,7 @@ void AssemblyDataFrame::OnWidthChange(wxCommandEvent& WXUNUSED(event))
 	if (m_cbxProportion->IsChecked() == true)
 	{
 		float width = m_txcWidth->GetValue();
-		
+
 		if (width >= SIZE_MAXIMUM_VALUE)
 		{
 			width = SIZE_MAXIMUM_VALUE;
@@ -217,10 +215,10 @@ void AssemblyDataFrame::OnWidthChange(wxCommandEvent& WXUNUSED(event))
 		{
 			width = SIZE_MINIMUM_VALUE;
 		}
-		
+
 		int n_height = (int)((width / m_assemblyWidth) * m_assemblyHeight);
 		int n_depth  = (int)((width / m_assemblyWidth) * m_assemblyDepth);
-		
+
 		m_txcHeight->SetValue(n_height);
 		m_txcDepth->SetValue(n_depth);
 	}
@@ -235,7 +233,7 @@ void AssemblyDataFrame::OnHeightChange(wxCommandEvent& WXUNUSED(event))
 	if (m_cbxProportion->IsChecked() == true)
 	{
 		float height = m_txcHeight->GetValue();
-		
+
 		if (height >= SIZE_MAXIMUM_VALUE)
 		{
 			height = SIZE_MAXIMUM_VALUE;
@@ -244,10 +242,10 @@ void AssemblyDataFrame::OnHeightChange(wxCommandEvent& WXUNUSED(event))
 		{
 			height = SIZE_MINIMUM_VALUE;
 		}
-		
+
 		int n_width  = (int)((height / m_assemblyHeight) * m_assemblyWidth);
 		int n_depth  = (int)((height / m_assemblyHeight) * m_assemblyDepth);
-		
+
 		m_txcWidth->SetValue(n_width);
 		m_txcDepth->SetValue(n_depth);
 	}
@@ -262,7 +260,7 @@ void AssemblyDataFrame::OnDepthChange(wxCommandEvent& WXUNUSED(event))
 	if (m_cbxProportion->IsChecked() == true)
 	{
 		float depth = m_txcDepth->GetValue();
-		
+
 		if (depth >= SIZE_MAXIMUM_VALUE)
 		{
 			depth = SIZE_MAXIMUM_VALUE;
@@ -271,10 +269,10 @@ void AssemblyDataFrame::OnDepthChange(wxCommandEvent& WXUNUSED(event))
 		{
 			depth = SIZE_MINIMUM_VALUE;
 		}
-		
+
 		int n_width  = (int)((depth / m_assemblyDepth) * m_assemblyWidth);
 		int n_height = (int)((depth / m_assemblyDepth) * m_assemblyHeight);
-		
+
 		m_txcWidth->SetValue(n_width);
 		m_txcHeight->SetValue(n_height);
 	}
--- src/include/AssemblyDataFrame.h
+++ src/include/AssemblyDataFrame.h
@@ -1,14 +1,27 @@
-#ifndef ASSEMBLYDATAFRAME_H
-#define ASSEMBLYDATAFRAME_H
+/**
+ * AssemblyDataFrame
+ * @name                AssemblyDataFrame.h
+ * @date                2008-10-05
+ * @author		Konstantin Balabin  <k.balabin@googlemail.com>
+ * @author		Patrick Kracht      <patrick.kracht@googlemail.com>
+ * @author		Thorsten Moll       <thorsten.moll@googlemail.com>
+ * @author		Valentin Kunz       <athostr@googlemail.com>
+ */
 
-#include <wx/textctrl.h>
-#include <wx/checkbox.h>
-#include <wx/stattext.h>
+#ifndef _ASSEMBLYDATAFRAME_H_
+#define _ASSEMBLYDATAFRAME_H_
+
 #include <wx/button.h>
+#include <wx/checkbox.h>
 #include <wx/frame.h>
 #include <wx/spinctrl.h>
+#include <wx/stattext.h>
 #include <wx/panel.h>
-#include "Assembly.h"
+
+//#include <wx/textctrl.h>
+
+
+class Assembly;
 
 class AssemblyDataFrame: public wxFrame
 {
@@ -16,43 +29,43 @@ public:
 
 	AssemblyDataFrame(wxWindow* parent = 0, wxWindowID id=1);
 	virtual ~AssemblyDataFrame();
-	
+
 	void fillFields(Assembly* assembly);
-	
+
 protected:
 
 private:
 	// assembly-sizes
 	float m_assemblyWidth, m_assemblyHeight, m_assemblyDepth;
-	
+
 	Assembly* m_assembly;
-	
+
 	wxPanel* m_panel;
-	
+
 	// text boxes
 	wxTextCtrl* m_txcName;
-	
+
 	wxSpinCtrl* m_txcWidth;
 	wxSpinCtrl* m_txcHeight;
 	wxSpinCtrl* m_txcDepth;
-	
+
 	wxCheckBox* m_cbxVisible;
 	wxCheckBox* m_cbxProportion;
-	
+
 	// buttons
 	wxButton* m_btnChange;
 	wxButton* m_btnCancel;
-	
+
 	// text labels
 	wxStaticText* m_stxName;
-	
+
 	wxStaticText* m_stxWidth;
 	wxStaticText* m_stxHeight;
 	wxStaticText* m_stxDepth;
-	
+
 	wxStaticText* m_stxVisible;
 	wxStaticText* m_stxProportion;
-	
+
 	// ids
 	static const long idtxcName;
 	static const long idSpcWidth;
@@ -62,15 +75,15 @@ private:
 	static const long idcbxProportion;
 	static const long idBtnChange;
 	static const long idBtnCancel;
-	
+
 	void OnChange(wxCommandEvent& WXUNUSED(event));
 	void OnCancel(wxCommandEvent& WXUNUSED(event));
 	void OnClose(wxCloseEvent& WXUNUSED(event));
 	void OnWidthChange(wxCommandEvent& WXUNUSED(event));
 	void OnHeightChange(wxCommandEvent& WXUNUSED(event));
 	void OnDepthChange(wxCommandEvent& WXUNUSED(event));
-	
+
 	DECLARE_EVENT_TABLE()
 };
 
-#endif
+#endif // _ASSEMBLYDATAFRAME_H_
