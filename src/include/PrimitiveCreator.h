76ae4e04b8ea9c375eb59e88991d8873f6cbfe22
Valentin Kunz <vale.kunz@googlemail.com>
2010-10-10 13:44:59 +0000

    Include/declaration cleanup for src/PrimitiveCreator.cpp

--- src/include/PrimitiveCreator.h
+++ src/include/PrimitiveCreator.h
@@ -8,28 +8,27 @@
  * @author		Valentin Kunz       <athostr@googlemail.com>
  */
 
-#ifndef _GNRPRIMITIVECREATOR_H_
-#define _GNRPRIMITIVECREATOR_H_
-
-#include "Assembly.h"
-#include "Vertex.h"
-#include "Face.h"
-#include "MaterialLibrary.h"
-#include "GlobalDefine.h"
+#ifndef _PRIMITIVECREATOR_H_
+#define _PRIMITIVECREATOR_H_
 
 #include <string>
+
+#include "GlobalDefine.h"
+
+class Assembly;
+class Vertex;
 
 class PrimitiveCreator
 {
 public:
 	PrimitiveCreator();
 	virtual ~PrimitiveCreator();
-	void createCuboid(const Vertex& position, const Vertex& angles, const Vertex& dimension,const string& matname = DEFAULT_IMPORT_COLOR);
-	void createSphere(float radius, const string& matname = DEFAULT_IMPORT_COLOR);
+	void createCuboid(const Vertex& position, const Vertex& angles, const Vertex& dimension,const std::string& matname = DEFAULT_IMPORT_COLOR);
+	void createSphere(float radius, const std::string& matname = DEFAULT_IMPORT_COLOR);
 	Assembly* getPrimitive();
 protected:
 private:
 	Assembly* m_primitive;
 };
 
-#endif // _GNRPRIMITIVECREATOR_H_
+#endif // _PRIMITIVECREATOR_H_
